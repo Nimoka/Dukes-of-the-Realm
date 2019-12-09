de0017bfb70defa36f472156029b3e1fe05f8a57
Nicolas Moignot <nicolas@moignot.net>
2019-12-09 17:49:23 +0100

    Add some constants to utils.Settings

--- src/utils/Settings.java
+++ src/utils/Settings.java
@@ -2,4 +2,24 @@ package utils;
 
 public abstract class Settings {
 	/*** CONSTANTS ************************************************/
+
+	public static int BOARD_HEIGHT = 24;
+	public static int BOARD_NB_CASTLES = 8;
+	public static int BOARD_WIDTH = 32;
+
+	public static int ENTITY_CATAPULT_ATTACK = 10;
+	public static int ENTITY_CATAPULT_HEALTH = 5;
+	public static int ENTITY_CATAPULT_PROD_COST = 1000;
+	public static int ENTITY_CATAPULT_PROD_TIME = 50;
+	public static int ENTITY_CATAPULT_SPEED = 1;
+	public static int ENTITY_KNIGHT_ATTACK = 5;
+	public static int ENTITY_KNIGHT_HEALTH = 3;
+	public static int ENTITY_KNIGHT_PROD_COST = 500;
+	public static int ENTITY_KNIGHT_PROD_TIME = 20;
+	public static int ENTITY_KNIGHT_SPEED = 6;
+	public static int ENTITY_PIKEMAN_ATTACK = 1;
+	public static int ENTITY_PIKEMAN_HEALTH = 1;
+	public static int ENTITY_PIKEMAN_PROD_COST = 100;
+	public static int ENTITY_PIKEMAN_PROD_TIME = 5;
+	public static int ENTITY_PIKEMAN_SPEED = 2;
 }
