0b43b70e60d7022920fda41ab755045283ee5e93
Nicolas Moignot <nicolas@moignot.net>
2019-12-16 20:41:21 +0100

    Add entities.* empty classes

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,6 +1,7 @@
 package game;
 
 import utils.Position;
+import utils.Settings;
 
 import java.util.ArrayList;
 
@@ -18,13 +19,29 @@ public class Board {
 	/*** METHODS **************************************************/
 
 	private void createCastles() {
+		this.castles = new ArrayList<>();
+		for (int i = 0; i < Settings.BOARD_NB_CASTLES; i++) {
+			Position position;
+			do {
+				position = Position.random(Settings.BOARD_WIDTH, Settings.BOARD_HEIGHT);
+			} while (!positionEmpty(position));
+			this.castles.add(new Castle(position));
+		}
 	}
 
 	public boolean positionEmpty(Position position) {
-		for (Castle castle: castles) {
+		for (Castle castle: this.castles) {
 			if (castle.getPosition().equals(position))
 				return false;
 		}
 		return true;
+	}
+
+	public String toString() {
+		String result = "Board { castles:\n";
+		for (Castle castle: this.castles)
+			result += '\t' + castle.toString() + '\n';
+		result += "}";
+		return result;
 	}
 }
---
+++ src/tests/Playground.java
@@ -0,0 +1,10 @@
+package tests;
+
+import game.*;
+
+public class Playground {
+	public static void main(String[] args) {
+		Board board = new Board();
+		System.out.println(board.toString());
+	}
+}
