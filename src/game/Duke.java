5d0871171f1aa07e378c8047cbe74c6d745cf56f
Nicolas Moignot <nicolas@moignot.net>
2020-01-03 16:19:36 +0100

    Add Duke base; add DukeType

--- src/game/Duke.java
+++ src/game/Duke.java
@@ -1,4 +1,33 @@
 package game;
 
+import utils.NameGenerator;
+
 public class Duke {
+	/*** VARIABLES ************************************************/
+
+	private String name;
+	private DukeType type;
+
+	/*** CONSTRUCTORS *********************************************/
+
+	public Duke(DukeType type) {
+		this.name = NameGenerator.random();
+		this.type = type;
+	}
+
+	/*** METHODS **************************************************/
+
+	public String toString() {
+		return "Duke { name: " + this.name + ", type: " + this.type + " }";
+	}
+
+	/*** GETTER/SETTER ********************************************/
+
+	public String getName() {
+		return this.name;
+	}
+
+	public DukeType getType() {
+		return this.type;
+	}
 }
--- src/game/DukeType.java
+++ src/game/DukeType.java
@@ -1,4 +1,6 @@
 package game;
 
 public enum DukeType {
+	PLAYER,
+	BARON;
 }
