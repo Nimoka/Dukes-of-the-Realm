8ec3ed286e1f12a768f01e6be77114a6229c8773
Nicolas Moignot <nicolas@moignot.net>
2020-01-03 17:45:23 +0100

    Add printRandomNames() in Playground

--- src/tests/Playground.java
+++ src/tests/Playground.java
@@ -1,10 +1,19 @@
 package tests;
 
 import game.*;
+import utils.NameGenerator;
 
 public class Playground {
-	public static void main(String[] args) {
+	public static void printBoard() {
 		Board board = new Board();
 		System.out.println(board.toString());
+	}
+
+	public static void printRandomNames(int nb) {
+		for (int i = 0; i < nb; i++)
+			System.out.println(NameGenerator.random());
+	}
+
+	public static void main(String[] args) {
 	}
 }
