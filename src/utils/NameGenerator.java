292b37d26fddc24472f7bfa246885ee32e484815
Nicolas Moignot <nicolas@moignot.net>
2020-01-03 17:07:09 +0100

    Add some names

--- src/utils/NameGenerator.java
+++ src/utils/NameGenerator.java
@@ -5,8 +5,8 @@ import java.util.List;
 import java.util.Random;
 
 public abstract class NameGenerator {
-	private static List<String> firstName = Arrays.asList("Jean", "Philippe", "Édouard", "Christophe", "Jean-Pierre", "Michel");
-	private static List<String> lastName = Arrays.asList("le Grand", "le Bon", "le Bref", "le Simple", "le Petit");
+	private static List<String> firstName = Arrays.asList("Jean", "Philippe", "Édouard", "Christophe", "Jean-Pierre", "Michel", "Théo", "Nicolas", "Laurent");
+	private static List<String> lastName = Arrays.asList("le Grand", "le Bon", "le Bref", "le Simple", "le Petit", "le Gentil", "le Moine", "le Tôt", "le Nez", "le Bleu");
 
 	public static String random() {
 		Random generator = new Random();
